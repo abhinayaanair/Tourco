f6d30b7512efcedb56638cb47c0543bc34fea621
abhinayaanair <abhinayapersonal11@gmail.com>
2024-06-13 09:28:27 +0530

    Add Project Files

---
+++ app.js
@@ -0,0 +1,112 @@
+if (!process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true })); //for data parsing
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+const dbUrl = process.env.ATLASDB_URL;
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+store.on("error", (error) => {
+  console.error("Error in Mongo session store:", error);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions)); // connect.sid cookie in session id
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+// app.get("/demouser",async(req,res)=>{
+// let fakeUser=new User({
+//   email:"student@gmail.com",
+//   username:"first-student"
+
+// });
+
+// let registeredUser= await User.register(fakeUser,"helloworld"); // register method ke andr (user,password);
+// res.send(registeredUser);
+// })
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+}); //* mtlb saari ki saari incoming req k saath match krdo
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went Wrong" } = err;
+  res.status(statusCode).render("error.ejs", { err });
+  // res.status(statusCode).send(message);
+});
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
